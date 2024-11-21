c87c7280e87ef3eba3970e83ea6294d8f1a741be
naresh1112 <44013454+naresh1112@users.noreply.github.com>
2024-11-21 12:40:21 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -14,41 +14,13 @@ import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
-  //  lateinit var toolbar: MaterialToolbar
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-/*
-        toolbar = findViewById(R.id.toolbar)
 
-        toolbar.overflowIcon = AppCompatResources.getDrawable(this, R.drawable.baseline_more_vert_24)
-
-        toolbar.setNavigationOnClickListener{
-
-            Toast.makeText(this, "menu icon is clicked", Toast.LENGTH_SHORT).show()
-        }
-
-        toolbar.setOnMenuItemClickListener{ item ->
-
-            when(item.itemId) {
-
-                R.id.share ->  Toast.makeText(this, "Share icon is clicked", Toast.LENGTH_SHORT).show()
-                R.id.edit -> Toast.makeText(this, "Edit icon is clicked", Toast.LENGTH_SHORT).show()
-                R.id.settings -> Toast.makeText(this, "Settings icon is clicked", Toast.LENGTH_SHORT).show()
-                R.id.notifications -> Toast.makeText(this, "Notification icon is clicked", Toast.LENGTH_SHORT).show()
-
-            }
-
-
-            return@setOnMenuItemClickListener true
-
-        }
-
-
- */
 
         // Set status bar color
        // window.statusBarColor = ContextCompat.getColor(this, R.color.blue500)
